3ad848ac4d09db9a85e9e188dfb067878b29dfc5
Nicholas C. Rivera <riveranicholasc@gmail.com>
2021-03-29 14:46:39 -0400

    Functions Lesson

---
+++ JavaScript/functions.js
@@ -0,0 +1,17 @@
+// Functions - Creating a function
+// DRY - Don't Repeat Yourself
+// Function converts an input into an output
+
+console.log(Number("123")); 
+
+function addTwo(x) { // Parameters x and y
+    
+    // Body
+    const y = x + 2;
+    
+    // Output
+    return y; // 
+}
+
+console.log(addTwo(3));
+
